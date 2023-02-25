a503c4c1fe9cf5ad1aa08fe50c66062be90e5b24
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-02-25 19:08:17 +0900

    Create Test1.js

---
+++ Test1.js
@@ -0,0 +1,8 @@
+i = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+a = parseInt(i[0]);
+e = i[1].split(" ");
+b = parseInt(e[0]);
+c = parseInt(e[1]);
+d = i[2];
+console.log(a+b+c+" "+d)
+//クソコードで草
