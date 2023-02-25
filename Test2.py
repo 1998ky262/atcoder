4f74ce22f0f611891b31d308e7e61afcfa99d0a6
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-02-25 20:20:02 +0900

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,4 +1,5 @@
 a = int(input())
 b,c = map(int,input().split(" "))
 str = input()
-print("{}{}".format(a+b+c," "+str)
+print("{}{}".format(a+b+c," "+str))
+# print("{}{}".format(a+b+c," "+str)になってました！(printの終了括弧がなかった)croud7さんありがとう！
