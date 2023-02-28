e01b4cb0815879abbaf2c6fa66670ea6348a2a2a
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-02-28 14:14:27 +0900

    Create PracticeA.cpp

---
+++ ABS/PracticeA.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    int b,c;
+    cin >> b >> c;
+    string s;
+    cin >> s;
+    cout << (a+b+c) << " " << s << endl;
+    return 0;
+}
