2b6d5d72542d7f0c75d0a5b6ddc31e9a4ca9ac6d
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-03-04 14:51:09 +0900

    Update A.py

--- Tohoku-University/A.py
+++ Tohoku-University/A.py
@@ -1,11 +1,11 @@
 N , K = map(int,input().split(" "))
 P = input().split(" ")
 ture_or_false= 0
-for h in range(len(p)):
+for h in range(len(P)):
    for c in range(len(P)):
      if P[c] < P[c+1]:
-       node1 = P[c]
-       node2 = P[c+1]
+       node1 = int(P[c])
+       node2 = int(P[c+1])
        if node1+node2 < K:
           P[c]=node2
           P[c+1]=node1
