8176eb8b492c859f7f0f67020ba248a507c53d6b
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-03-04 14:44:47 +0900

    Update A.py

--- Tohoku-University/A.py
+++ Tohoku-University/A.py
@@ -1,5 +1,18 @@
 N , K = map(int,input().split(" "))
 P = input().split(" ")
-for c in range(len(p)):
-  if P[c] < P[c+1]:
-    #しね
+ture_or_false= 0
+for h in range(len(p)):
+   for c in range(len(p)):
+     if P[c] < P[c+1]:
+       node1 = P[c]
+       node2 = P[c+1]
+       if node1+node2 < K:
+          P[c]=node2
+          P[c+1]=node1
+       else:
+         ture_or_false=1
+         break
+if ture_or_false==0:
+  print("Yes")
+else:
+  print("No")
