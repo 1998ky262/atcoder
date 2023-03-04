b578d46e5ca72c5cd0923019e8cbe89f130fc9ac
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-03-04 15:13:51 +0900

    Update A.py

--- Tohoku-University/A.py
+++ Tohoku-University/A.py
@@ -1,18 +1,20 @@
+def can_sort_permutation(P, K):
+    # Pを昇順に並べ替えたリストを作成する
+    sorted_P = sorted(P)
+
+    # Pを昇順に並べ替えるための操作を行う
+    for i in range(len(P)):
+        for j in range(i + 1, len(P)):
+            if P[i] + P[j] <= K:
+                # P[i]とP[j]を入れ替えて、Pを更新する
+                P[i], P[j] = P[j], P[i]
+
+    # Pが昇順に並べ替えられたかどうかを判定する
+    return P == sorted_P
 N , K = map(int,input().split(" "))
 P = input().split(" ")
-ture_or_false= 0
-for h in range(len(P)):
-   for c in range(len(P)):
-     if P[c] < P[c+1]:
-       node1 = int(P[c])
-       node2 = int(P[c+1])
-       if node1+node2 < K:
-          P[c]=node2
-          P[c+1]=node1
-       else:
-         ture_or_false=1
-         break
-if ture_or_false==0:
+
+if can_sort_permutation(P,K)
   print("Yes")
 else:
   print("No")
